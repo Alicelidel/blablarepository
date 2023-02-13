895955bc14ab7f406cf4a0da4c7e4c07b311ab55
Elizaveta Iashchak <jamierenoi@gmail.com>
2023-02-13 17:57:55 +0100

    Prime number func and prime number func test

--- lessons/one.py
+++ lessons/one.py
@@ -1,2 +1,15 @@
 def inna_func(args: list):
     return len(args) % 2 == 0
+
+
+def lera_func(a: list[int]):
+    for x in a:
+        if not isinstance(x, int):
+            return Exception('not a number')
+        for i in range(2, x - 1):
+            if x % i == 0:
+                break
+        else:
+            return True
+    else:
+        return False
--- test/test_one.py
+++ test/test_one.py
@@ -1,4 +1,4 @@
-from lessons.one import inna_func
+from lessons.one import inna_func, lera_func
 import pytest
 
 
@@ -13,4 +13,18 @@ import pytest
 )
 def test_returns_true(array):
     result = inna_func(array)
+    assert result is True
+
+
+@pytest.mark.parametrize(
+    "array",
+    [
+        ([]),
+        ([1, 2, 3]),
+        (['a', 'b']),
+        ([[], []]),
+    ]
+)
+def test_prime(array):
+    result = lera_func(array)
     assert result is True
