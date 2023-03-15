6784e6b17a80762cb37dcff4285d4c577ad7ab2b
val <valeryvalyaeva@gmail.com>
2023-03-15 19:00:53 +0300

    add Exceptions.py file and all exceptions are replaced in this file. All files are updated

--- lessons/three.py
+++ lessons/three.py
@@ -1,5 +1,5 @@
-class NotAStringException(Exception):
-    pass
+from lessons.Exceptions import NotAStringException
+
 
 def inna_func_add_sum_to_the_middle(args):
     if not isinstance(args, str):
@@ -9,5 +9,3 @@ def inna_func_add_sum_to_the_middle(args):
         return ''.join((args[:(len(args) // 2)], str(len(args)), args[(len(args) // 2):]))
     else:
         return False
-
-
