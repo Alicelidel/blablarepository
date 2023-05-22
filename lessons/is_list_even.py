6beb164628e16cccce5893d1f655f2ac87ff8d55
inna67rus@gmail.com <inna67rus@gmail.com>
2023-05-22 21:03:28 +0300

    Rename the test file, and rename the variable in the function

--- lessons/is_list_even.py
+++ lessons/is_list_even.py
@@ -2,10 +2,10 @@ from lessons.Exceptions import NotAListException
 
 
 # Defining function to check if the length of the list is even
-def is_list_even(input_string: list):
+def is_list_even(input_list: list):
     # Check if input_string is a list
-    if not isinstance(input_string, list):
+    if not isinstance(input_list, list):
         # Raise an exception if input_string is not a list
         return NotAListException('not a list')
     # Check if the length of the list is even
-    return len(input_string) % 2 == 0
+    return len(input_list) % 2 == 0
